5d532a366f3d15cb46423fe887ad08d2fbcab792
Th0rn-dev <solnek33@gmail.com>
2024-01-04 13:41:38 +0300

    Сondition replaced with assertion

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,21 +1,15 @@
 from pg_1.capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
+assert capitalize('hello') == 'Hello', 'Функция работает неверно!'
 
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+assert capitalize('') == '', 'Функция работает неверно!'
 
-if capitalize('3') != '3':
-    raise Exception('Функция работает неверно!')
+assert capitalize('3') == '3', 'Функция работает неверно!'
 
-if capitalize('%') != '%':
-    raise Exception('Функция работает неверно!')
+assert capitalize('%') == '%', 'Функция работает неверно!'
 
-if capitalize(3) != 3:
-    raise Exception('Функция работает неверно!')
+assert capitalize(3) == 3, 'Функция работает неверно!'
 
-if capitalize(2 / 0) is not None:
-    raise Exception('Деление на ноль в аргументе!')
+assert capitalize(2 / 0) is not None, 'Деление на ноль в аргументе!'
 
 print('Все тесты пройдены')
